290c50b99bef6baa24cecdd2fe9ee34c2510eb82
Caroline <caroline_mendez@baylor.edu>
2020-08-30 22:45:06 -0500

    login screen

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:tasker/screens/login_screen.dart';
+import 'package:tasker/screens/signup_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tasker',
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+      routes: {
+        LoginScreen.id: (context) => LoginScreen(),
+        SignupScreen.id: (context) => SignupScreen(),
+      },
+    );
+  }
+}
---
+++ lib/screens/signup_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class SignupScreen extends StatefulWidget {
+  static final String id = 'signup_screen';
+
+  @override
+  _SignupScreenState createState() => _SignupScreenState();
+}
+
+class _SignupScreenState extends State<SignupScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Sign Up For Tasker',
+                style: TextStyle(fontSize: 30.0),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
